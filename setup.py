88b20bb66ab8fa77fbd72063e975f46db1fd8df2
Ole André Vadla Ravnås <oleavr@gmail.com>
2019-08-28 16:24:15 +0200

    3.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="frida-tools",
-    version="3.0.0",
+    version="3.0.1",
     description="Frida CLI tools",
     long_description="CLI tools for [Frida](https://www.frida.re).",
     long_description_content_type="text/markdown",
