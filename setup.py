5477768c77f80b85ff16a609a0f73a6dc2f3d284
Ole André Vadla Ravnås <oleavr@gmail.com>
2020-08-04 13:23:26 +0200

    Explicitly declare Python 3.7 and 3.8 as supported

--- setup.py
+++ setup.py
@@ -47,6 +47,8 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: JavaScript",
         "Topic :: Software Development :: Debuggers",
         "Topic :: Software Development :: Libraries :: Python Modules"
