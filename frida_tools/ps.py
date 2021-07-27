712c8c857855e50f8e281ba2ab1e2cab46726bcc
Ole André Vadla Ravnås <oleavr@gmail.com>
2021-07-27 12:15:27 +0200

    Enhance frida-ps to show icons when using iTerm2

--- frida_tools/ps.py
+++ frida_tools/ps.py
@@ -3,7 +3,13 @@ from __future__ import unicode_literals, print_function
 
 
 def main():
+    from base64 import b64encode
     import json
+    import math
+    import platform
+    import sys
+    import termios
+    import tty
 
     from frida_tools.application import ConsoleApplication
 
@@ -23,6 +29,7 @@ def main():
             self._list_only_applications = options.list_only_applications
             self._include_all_applications = options.include_all_applications
             self._output_format = options.output_format
+            self._terminal_type, self._icon_size = self._detect_terminal()
 
         def _usage(self):
             return "usage: %prog [options]"
@@ -34,8 +41,13 @@ def main():
                 self._list_processes()
 
         def _list_processes(self):
+            if self._output_format == 'text' and self._terminal_type == 'iterm2':
+                scope = 'full'
+            else:
+                scope = 'minimal'
+
             try:
-                processes = self._device.enumerate_processes()
+                processes = self._device.enumerate_processes(scope=scope)
             except Exception as e:
                 self._update_status("Failed to enumerate processes: %s" % e)
                 self._exit(1)
@@ -44,13 +56,28 @@ def main():
             if self._output_format == 'text':
                 if len(processes) > 0:
                     pid_column_width = max(map(lambda p: len("%d" % p.pid), processes))
-                    name_column_width = max(map(lambda p: len(p.name), processes))
+                    icon_width = max(map(compute_icon_width, processes))
+                    name_column_width = icon_width + max(map(lambda p: len(p.name), processes))
+
                     header_format = "%" + str(pid_column_width) + "s  %s"
                     self._print(header_format % ("PID", "Name"))
                     self._print("%s  %s" % (pid_column_width * "-", name_column_width * "-"))
+
                     line_format = "%" + str(pid_column_width) + "d  %s"
+                    name_format = "%-" + str(name_column_width - icon_width) + "s"
+
                     for process in sorted(processes, key=cmp_to_key(compare_processes)):
-                        self._print(line_format % (process.pid, process.name))
+                        if icon_width != 0:
+                            icons = process.parameters.get('icons', None)
+                            if icons is not None:
+                                icon = self._render_icon(icons[0])
+                            else:
+                                icon = "   "
+                            name = icon + " " + name_format % process.name
+                        else:
+                            name = name_format % process.name
+
+                        self._print(line_format % (process.pid, name))
                 else:
                     self._log('error', "No running processes.")
             elif self._output_format == 'json':
@@ -62,8 +89,13 @@ def main():
             self._exit(0)
 
         def _list_applications(self):
+            if self._output_format == 'text' and self._terminal_type == 'iterm2':
+                scope = 'full'
+            else:
+                scope = 'minimal'
+
             try:
-                applications = self._device.enumerate_applications()
+                applications = self._device.enumerate_applications(scope=scope)
             except Exception as e:
                 self._update_status("Failed to enumerate applications: %s" % e)
                 self._exit(1)
@@ -75,7 +107,8 @@ def main():
             if self._output_format == 'text':
                 if len(applications) > 0:
                     pid_column_width = max(map(lambda app: len("%d" % app.pid), applications))
-                    name_column_width = max(map(lambda app: len(app.name), applications))
+                    icon_width = max(map(compute_icon_width, applications))
+                    name_column_width = icon_width + max(map(lambda app: len(app.name), applications))
                     identifier_column_width = max(map(lambda app: len(app.identifier), applications))
 
                     header_format = "%" + str(pid_column_width) + "s  " + \
@@ -83,15 +116,26 @@ def main():
                         "%-" + str(identifier_column_width) + "s"
                     self._print(header_format % ("PID", "Name", "Identifier"))
                     self._print("%s  %s  %s" % (pid_column_width * "-", name_column_width * "-", identifier_column_width * "-"))
-                    line_format = "%" + str(pid_column_width) + "s  " + \
-                        "%-" + str(name_column_width) + "s  " + \
-                        "%-" + str(identifier_column_width) + "s"
+
+                    line_format = "%" + str(pid_column_width) + "s  %s  %-" + str(identifier_column_width) + "s"
+                    name_format = "%-" + str(name_column_width - icon_width) + "s"
 
                     for app in sorted(applications, key=cmp_to_key(compare_applications)):
-                        if app.pid == 0:
-                            self._print(line_format % ("-", app.name, app.identifier))
+                        if icon_width != 0:
+                            icons = app.parameters.get('icons', None)
+                            if icons is not None:
+                                icon = self._render_icon(icons[0])
+                            else:
+                                icon = "   "
+                            name = icon + " " + name_format % app.name
                         else:
-                            self._print(line_format % (app.pid, app.name, app.identifier))
+                            name = name_format % app.name
+
+                        if app.pid == 0:
+                            self._print(line_format % ("-", name, app.identifier))
+                        else:
+                            self._print(line_format % (app.pid, name, app.identifier))
+
                 elif self._include_all_applications:
                     self._log('error', "No installed applications.")
                 else:
@@ -104,6 +148,63 @@ def main():
                 self._print(json.dumps(result, sort_keys=False, indent=2))
 
             self._exit(0)
+
+        def _render_icon(self, icon):
+            return "\033]1337;File=inline=1;width={}px;height={}px;:{}\007".format(self._icon_size,
+                                                                                   self._icon_size,
+                                                                                   b64encode(icon['image']).decode('ascii'))
+
+        def _detect_terminal(self):
+            icon_size = 0
+
+            if not self._have_terminal or self._plain_terminal or platform.system() != 'Darwin':
+                return ('simple', icon_size)
+
+            fd = sys.stdin.fileno()
+            old_attributes = termios.tcgetattr(fd)
+            try:
+                tty.setraw(fd)
+                new_attributes = termios.tcgetattr(fd)
+                new_attributes[3] = new_attributes[3] & ~termios.ICANON & ~termios.ECHO
+                termios.tcsetattr(fd, termios.TCSANOW, new_attributes)
+
+                sys.stdout.write("\033[1337n")
+                sys.stdout.write("\033[5n")
+                sys.stdout.flush()
+
+                response = self._read_terminal_response('n')
+                if response not in ("0", "3"):
+                    self._read_terminal_response('n')
+
+                    if response.startswith("ITERM2 "):
+                        version_tokens = response.split(" ", 1)[1].split(".", 2)
+                        if len(version_tokens) >= 2 and int(version_tokens[0]) >= 3:
+                            sys.stdout.write("\033[14t")
+                            sys.stdout.flush()
+                            height_in_pixels = int(self._read_terminal_response('t').split(";")[1])
+
+                            sys.stdout.write("\033[18t")
+                            sys.stdout.flush()
+                            height_in_cells = int(self._read_terminal_response('t').split(";")[1])
+
+                            icon_size = math.ceil((height_in_pixels / height_in_cells) * 1.77)
+
+                            return ('iterm2', icon_size)
+
+                return ('simple', icon_size)
+            finally:
+                termios.tcsetattr(fd, termios.TCSANOW, old_attributes)
+
+        def _read_terminal_response(self, terminator):
+            sys.stdin.read(1)
+            sys.stdin.read(1)
+            result = ""
+            while True:
+                ch = sys.stdin.read(1)
+                if ch == terminator:
+                    break
+                result += ch
+            return result
 
 
     def compare_applications(a, b):
@@ -123,12 +224,26 @@ def main():
 
 
     def compare_processes(a, b):
-        if a.name > b.name:
-            return 1
-        elif a.name < b.name:
+        a_has_icon = 'icons' in a.parameters
+        b_has_icon = 'icons' in b.parameters
+        if a_has_icon == b_has_icon:
+            if a.name > b.name:
+                return 1
+            elif a.name < b.name:
+                return -1
+            else:
+                return 0
+        elif a_has_icon:
             return -1
         else:
-            return 0
+            return 1
+
+
+    def compute_icon_width(item):
+        for icon in item.parameters.get('icons', []):
+            if icon['format'] == 'png':
+                return 4
+        return 0
 
 
     def cmp_to_key(mycmp):
