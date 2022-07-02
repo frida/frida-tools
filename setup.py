32a51eeb6f1cdac8cfcb85f21f5794684c8fdbdd
Ole André Vadla Ravnås <oleavr@gmail.com>
2022-07-02 13:31:58 +0200

    10.7.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ assert len(agents) > 0, "Agents not compiled; run “npm install && npm run buil
 
 setup(
     name="frida-tools",
-    version="10.6.2",
+    version="10.7.0",
     description="Frida CLI tools",
     long_description="CLI tools for [Frida](https://frida.re).",
     long_description_content_type="text/markdown",
