0564d82435f947de86763bdb9effa9c685b33e73
Aman Karmani <aman@tmm1.net>
2022-10-10 16:21:23 -0700

    show device name and OS in frida-ls-devices

--- frida_tools/lsd.py
+++ frida_tools/lsd.py
@@ -19,9 +19,25 @@ def main() -> None:
                 self._update_status(f"Failed to enumerate devices: {e}")
                 self._exit(1)
                 return
+            device_name = {}
+            device_os = {}
+            for device in devices:
+                device_name[device.id] = device.name
+                try:
+                    params = device.query_system_parameters()
+                    device_name[device.id] = params.get("name", device.name)
+                    os = params["os"]
+                    version = os.get("version")
+                    if version != None:
+                        device_os[device.id] = os["name"] + " " + version
+                    else:
+                        device_os[device.id] = os["name"]
+                except frida.ServerNotRunningError:
+                    pass
             id_column_width = max(map(lambda device: len(device.id) if device.id is not None else 0, devices))
             type_column_width = max(map(lambda device: len(device.type) if device.type is not None else 0, devices))
-            name_column_width = max(map(lambda device: len(device.name) if device.name is not None else 0, devices))
+            name_column_width = max(map(lambda name: len(name) if name is not None else 0, device_name.values()))
+            os_column_width = max(map(lambda os: len(os) if os is not None else 0, device_os.values()))
             header_format = (
                 "%-"
                 + str(id_column_width)
@@ -31,10 +47,15 @@ def main() -> None:
                 + "s  "
                 + "%-"
                 + str(name_column_width)
+                + "s  "
+                + "%-"
+                + str(os_column_width)
                 + "s"
             )
-            self._print(header_format % ("Id", "Type", "Name"))
-            self._print(f"{id_column_width * '-'}  {type_column_width * '-'}  {name_column_width * '-'}")
+            self._print(header_format % ("Id", "Type", "Name", "OS"))
+            self._print(
+                f"{id_column_width * '-'}  {type_column_width * '-'}  {name_column_width * '-'}  {os_column_width * '-'}"
+            )
             line_format = (
                 "%-"
                 + str(id_column_width)
@@ -44,10 +65,15 @@ def main() -> None:
                 + "s  "
                 + "%-"
                 + str(name_column_width)
+                + "s  "
+                + "%-"
+                + str(os_column_width)
                 + "s"
             )
             for device in sorted(devices, key=functools.cmp_to_key(compare_devices)):
-                self._print(line_format % (device.id, device.type, device.name))
+                self._print(
+                    line_format % (device.id, device.type, device_name.get(device.id), device_os.get(device.id, ""))
+                )
             self._exit(0)
 
     def compare_devices(a: frida.core.Device, b: frida.core.Device) -> int:
