63cd957ae2dbe93df5c5670afafaedd5d54304af
Maximilian Hils <git@maximilianhils.com>
2023-02-27 18:28:27 +0100

    binaryxml: fix stringpool insertion padding

--- frida_tools/apk.py
+++ frida_tools/apk.py
@@ -264,7 +264,7 @@ class StringPool:
 
         string = None
         if self.utf8:
-            # Ignore UTF-16 length
+            # Ignore number of characters
             n = struct.unpack("<B", self.stream.read(1))[0]
             if n & 0x80:
                 n = ((n & 0x7F) << 8) | struct.unpack("<B", self.stream.read(1))[0]
@@ -299,10 +299,11 @@ class StringPool:
         offset = len(chunk_data) - 8 - self.strings_offset + 4
 
         if self.utf8:
-            # UTF-16 length (ignored)
+            assert len(add.encode("utf-8")) < 128  # multi-byte len strings not supported yet
+            # the length of the string in characters
             chunk_data.extend(struct.pack("<B", len(add)))
-            # UTF-8 length
-            chunk_data.extend(struct.pack("<B", len(add)))
+            # the length of the UTF-8 encoding of the string in bytes
+            chunk_data.extend(struct.pack("<B", len(add.encode("utf-8"))))
 
             chunk_data.extend(add.encode("utf-8"))
             # Insert a UTF-8 NUL
@@ -312,6 +313,9 @@ class StringPool:
             chunk_data.extend(add.encode("utf-16le"))
             # Insert a UTF-16 NUL
             chunk_data.extend([0, 0])
+
+        # pad to a multiple of 4 bytes
+        chunk_data.extend([0] * (4 - len(chunk_data) % 4))
 
         # Insert a new offset at the end of the existing offsets
         chunk_data[end : end + 4] = struct.pack("<I", offset)
