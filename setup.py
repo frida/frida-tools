d37df2e2c2e4b39c0a0fc89a34242e34f0730b7f
Ole André Vadla Ravnås <oleavr@gmail.com>
2023-05-03 12:37:03 +0200

    12.1.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ assert len(agents) > 0, "Agents not compiled; run “npm install && npm run buil
 
 setup(
     name="frida-tools",
-    version="12.1.1",
+    version="12.1.2",
     description="Frida CLI tools",
     long_description="CLI tools for [Frida](https://frida.re).",
     long_description_content_type="text/markdown",
