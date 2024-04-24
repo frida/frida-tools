fdcbf669af24b3d95f8347b858ccdd1bdcd216fd
Ole André Vadla Ravnås <oleavr@gmail.com>
2024-04-24 13:00:00 +0200

    meson: Fail early if npm is missing
    Also ensure Node.js is recent, and refactor a little bit.

--- agents/build.py
+++ agents/build.py
@@ -6,7 +6,21 @@ import sys
 from pathlib import Path
 
 
-def build(inputs, output_js, priv_dir):
+def main(argv: list[str]):
+    npm = argv[1]
+    paths = [Path(p).resolve() for p in argv[2:]]
+    inputs = paths[:-2]
+    output_js = paths[-2]
+    priv_dir = paths[-1]
+
+    try:
+        build(npm, inputs, output_js, priv_dir)
+    except Exception as e:
+        print(e, file=sys.stderr)
+        sys.exit(1)
+
+
+def build(npm: Path, inputs: list[Path], output_js: Path, priv_dir: Path):
     pkg_file = next((f for f in inputs if f.name == "package.json"))
     pkg_parent = pkg_file.parent
     entrypoint = inputs[0].relative_to(pkg_parent)
@@ -19,44 +33,18 @@ def build(inputs, output_js, priv_dir):
         if not dstdir.exists():
             dstdir.mkdir()
 
-        shutil.copyfile(srcfile, dstfile)
+        shutil.copy(srcfile, dstfile)
 
-    npm = os.environ.get("NPM", "npm")
-    try:
-        subprocess.run([npm, "install"], capture_output=True, cwd=priv_dir, check=True)
-    except Exception as e:
-        message = "\n".join(
-            [
-                "",
-                "***",
-                f"Failed to build {inputs[0].name}:",
-                "\t" + str(e),
-                "This is most likely because Node.js is not installed.",
-                "We need it for processing JavaScript code at build-time.",
-                "Check PATH or set NPM to the absolute path of your npm binary.",
-                "***\n",
-            ]
-        )
-        raise EnvironmentError(message)
+    subprocess.run([npm, "install"], capture_output=True, cwd=priv_dir, check=True)
 
-    frida_compile = Path("node_modules") / ".bin" / ("frida-compile" + script_suffix())
+    frida_compile = Path("node_modules") / ".bin" / f"frida-compile{script_suffix()}"
 
     subprocess.run([frida_compile, entrypoint, "-c", "-o", output_js], cwd=priv_dir, check=True)
 
 
-def script_suffix():
-    build_os = platform.system().lower()
-    return ".cmd" if build_os == "windows" else ""
+def script_suffix() -> str:
+    return ".cmd" if platform.system() == "Windows" else ""
 
 
 if __name__ == "__main__":
-    paths = [Path(p).resolve() for p in sys.argv[1:]]
-    inputs = paths[:-2]
-    output_js = paths[-2]
-    priv_dir = paths[-1]
-
-    try:
-        build(inputs, output_js, priv_dir)
-    except Exception as e:
-        print(e, file=sys.stderr)
-        sys.exit(1)
+    main(sys.argv)
